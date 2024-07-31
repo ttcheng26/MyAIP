4ab205b5a4c03b00d21d43da61c6c9bb2bc9e4fb
ttcheng26 <alan870205@gmail.com>
2024-08-01 01:22:43 +0800

    update

--- HW3/HW3 61247046S.py
+++ HW3/HW3 61247046S.py
@@ -8,38 +8,45 @@ import os
 
 
 win = tk.Tk()
-win.title("AIP 61247046S") # Title
-win.geometry("1150x800+300+20") # screen size
-win.config(bg="#323232") # background color
+win.title("AIP 61247046S")  # Title
+win.geometry("1150x800+300+20")  # screen size
+win.config(bg="#323232")  # background color
 # win.iconbitmap("photo.ico") # icon
 
-def cv_imread(filePath): # 避免檔名中文發生錯誤
-    cv_img=cv2.imdecode(np.fromfile(filePath,dtype=np.uint8), -1)
+
+def cv_imread(filePath):  # 避免檔名中文發生錯誤
+    cv_img = cv2.imdecode(np.fromfile(filePath, dtype=np.uint8), -1)
     return cv_img
 
-def show(): # 顯示照片
+
+def show():  # 顯示照片
     global img_path
-    img_path = filedialog.askopenfilename(title="選擇", filetypes=[('png', '*.png'), ('jpg', '*.jpg'), ('gif', '*.gif'), ('bmp', '*.bmp'), ('ppm', '*.ppm')])
+    img_path = filedialog.askopenfilename(title="選擇", filetypes=[(
+        'png', '*.png'), ('jpg', '*.jpg'), ('gif', '*.gif'), ('bmp', '*.bmp'), ('ppm', '*.ppm')])
     while img_path != '':
         img = cv_imread(img_path)
         global cv_img
         cv_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-        tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(cv_img, (320, 320))))
+        tk_img = ImageTk.PhotoImage(
+            image=Image.fromarray(cv2.resize(cv_img, (320, 320))))
         canvas1.delete('all')
-        canvas1.create_image(0,0, anchor='nw', image=tk_img)
+        canvas1.create_image(0, 0, anchor='nw', image=tk_img)
         canvas1.tk_img = tk_img
         break
 
-def show_gray(): # 顯示灰階
+
+def show_gray():  # 顯示灰階
     global img_path
     # img_path = filedialog.askopenfilename(title="選擇",filetypes=[('png', '*.png'), ('jpg', '*.jpg'), ('gif', '*.gif'), ('bmp', '*.bmp'), ('ppm', '*.ppm')])
     img = cv_imread(img_path)
     global cv_img
     cv_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(cv_img, (320, 320))))
+    tk_img = ImageTk.PhotoImage(
+        image=Image.fromarray(cv2.resize(cv_img, (320, 320))))
     canvas1.delete('all')
-    canvas1.create_image(0,0, anchor='nw', image=tk_img)
+    canvas1.create_image(0, 0, anchor='nw', image=tk_img)
     canvas1.tk_img = tk_img
+
 
 def flip():  # 影像翻轉
     # global img_path
@@ -50,7 +57,7 @@ def flip():  # 影像翻轉
     img2 = cv2.flip(cv2.resize(cv_img, (320, 320)), -1)
     tk_img = ImageTk.PhotoImage(image=Image.fromarray(img2))
     canvas1.delete('all')
-    canvas1.create_image(0,0, anchor='nw', image=tk_img)
+    canvas1.create_image(0, 0, anchor='nw', image=tk_img)
     canvas1.tk_img = tk_img
 
 
@@ -60,6 +67,7 @@ def gaussian():
     labelExample = tk.Label(newWindow, text="請輸入標準差")
     labelExample.config(font=("微軟正黑體", 13, "bold"))
     num = tk.Entry(newWindow, width=15)
+
     def execute_and_quit():
         global cv_img
         G = 256
@@ -95,15 +103,17 @@ def gaussian():
 
             # Check for convergence
             if np.all(np.abs(noisy_image - f_prime) < 1e-5) and np.all(
-                   np.abs(noisy_image_neighbor - f_prime_neighbor) < 1e-5):
+                    np.abs(noisy_image_neighbor - f_prime_neighbor) < 1e-5):
                 break
 
             if np.all(np.abs(blank_image - f_prime2) < 1e-5) and np.all(
-                     np.abs(blank_image_neighbor - f_prime_neighbor2) < 1e-5):
+                    np.abs(blank_image_neighbor - f_prime_neighbor2) < 1e-5):
                 break
 
-        tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(noisy_image, (320, 320))))
-        tk_img2 = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(blank_image, (320, 320))))
+        tk_img = ImageTk.PhotoImage(image=Image.fromarray(
+            cv2.resize(noisy_image, (320, 320))))
+        tk_img2 = ImageTk.PhotoImage(image=Image.fromarray(
+            cv2.resize(blank_image, (320, 320))))
         canvas2.delete('all')
         canvas3.delete('all')
         canvas2.create_image(0, 0, anchor='nw', image=tk_img2)
@@ -113,7 +123,8 @@ def gaussian():
 
         newWindow.destroy()
 
-    buttonExample = tk.Button(newWindow, text="產生高斯雜訊", command=execute_and_quit)
+    buttonExample = tk.Button(newWindow, text="產生高斯雜訊",
+                              command=execute_and_quit)
     buttonExample.config(font=("微軟正黑體", 10, "bold"))
 
     labelExample.pack()
@@ -127,42 +138,47 @@ def salt_and_pepper():
     labelExample2 = tk.Label(newWindow2, text="請輸入百分比")
     labelExample2.config(font=("微軟正黑體", 13, "bold"))
     num2 = tk.Entry(newWindow2, width=15)
+
     def execute_and_quit2():
-         global cv_img
-         height, width = cv_img.shape
-         global noisy_image
-         noisy_image = cv_img.copy()
-         # Create a blank image (black)
-         global blank_image
-         blank_image = np.full((height, width), 255, dtype=np.uint8)
-         # blank_image = np.zeros((height, width), dtype=np.uint8)
-         p = int(num2.get())
+        global cv_img
+        height, width = cv_img.shape
+        global noisy_image
+        noisy_image = cv_img.copy()
+        # Create a blank image (black)
+        global blank_image
+        blank_image = np.full((height, width), 255, dtype=np.uint8)
+        # blank_image = np.zeros((height, width), dtype=np.uint8)
+        p = int(num2.get())
 
-         num_pixels = int(height * width * p/100)
+        num_pixels = int(height * width * p/100)
 
-         # Generate random pixel coordinates to add impulse noise
-         impulse_coordinates = [np.random.randint(0, height, num_pixels), np.random.randint(0, width, num_pixels)]
+        # Generate random pixel coordinates to add impulse noise
+        impulse_coordinates = [np.random.randint(
+            0, height, num_pixels), np.random.randint(0, width, num_pixels)]
 
-         # Set randomly selected pixels to either pure black (0) or pure white (255)
-         for i in range(num_pixels):
-             x, y = impulse_coordinates[0][i], impulse_coordinates[1][i]
-             if np.random.random() < 0.5:
-                 noisy_image[x, y], blank_image[x,y] = 0, 0  # Set to black
-             else:
-                 noisy_image[x, y], blank_image[x,y] = 255, 255  # Set to white
+        # Set randomly selected pixels to either pure black (0) or pure white (255)
+        for i in range(num_pixels):
+            x, y = impulse_coordinates[0][i], impulse_coordinates[1][i]
+            if np.random.random() < 0.5:
+                noisy_image[x, y], blank_image[x, y] = 0, 0  # Set to black
+            else:
+                noisy_image[x, y], blank_image[x, y] = 255, 255  # Set to white
 
-         tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(noisy_image, (320, 320))))
-         tk_img2 = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(blank_image, (320, 320))))
-         canvas2.delete('all')
-         canvas3.delete('all')
-         canvas2.create_image(0, 0, anchor='nw', image=tk_img2)
-         canvas3.create_image(0, 0, anchor='nw', image=tk_img)
-         canvas2.tk_img = tk_img2
-         canvas3.tk_img = tk_img
+        tk_img = ImageTk.PhotoImage(image=Image.fromarray(
+            cv2.resize(noisy_image, (320, 320))))
+        tk_img2 = ImageTk.PhotoImage(image=Image.fromarray(
+            cv2.resize(blank_image, (320, 320))))
+        canvas2.delete('all')
+        canvas3.delete('all')
+        canvas2.create_image(0, 0, anchor='nw', image=tk_img2)
+        canvas3.create_image(0, 0, anchor='nw', image=tk_img)
+        canvas2.tk_img = tk_img2
+        canvas3.tk_img = tk_img
 
-         newWindow2.destroy()
+        newWindow2.destroy()
 
-    buttonExample2 = tk.Button(newWindow2, text="產生椒鹽雜訊", command=execute_and_quit2)
+    buttonExample2 = tk.Button(
+        newWindow2, text="產生椒鹽雜訊", command=execute_and_quit2)
     buttonExample2.config(font=("微軟正黑體", 10, "bold"))
 
     labelExample2.pack()
@@ -170,8 +186,7 @@ def salt_and_pepper():
     buttonExample2.pack()
 
 
-
-def histogram(): # 顯示直方圖
+def histogram():  # 顯示直方圖
     global cv_img
     img3 = cv2.equalizeHist(cv_img)
     plt.figure()
@@ -180,40 +195,43 @@ def histogram(): # 顯示直方圖
     plt.close()
     global hist1
     hist1 = cv_imread("hist1.png")
-    tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(hist1, (320, 320))))
+    tk_img = ImageTk.PhotoImage(
+        image=Image.fromarray(cv2.resize(hist1, (320, 320))))
     canvas4.delete('all')
-    canvas4.create_image(0,0, anchor='nw', image=tk_img)
+    canvas4.create_image(0, 0, anchor='nw', image=tk_img)
     canvas4.tk_img = tk_img
     os.remove("hist1.png")
 
     global blank_image
-    img4, bins = np.histogram(blank_image.flatten(), bins=256, range=[0,256])
+    img4, bins = np.histogram(blank_image.flatten(), bins=256, range=[0, 256])
     plt.figure()
     # plt.hist(img4.ravel(), 256, [0, 255], color="black")
-    plt.plot(img4, color = 'black')
+    plt.plot(img4, color='black')
     plt.savefig("hist2.png")
     plt.close()
     global hist2
     hist2 = cv_imread("hist2.png")
-    tk_img2 = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(hist2, (320, 320))))
+    tk_img2 = ImageTk.PhotoImage(
+        image=Image.fromarray(cv2.resize(hist2, (320, 320))))
     canvas5.delete('all')
-    canvas5.create_image(0,0, anchor='nw', image=tk_img2)
+    canvas5.create_image(0, 0, anchor='nw', image=tk_img2)
     canvas5.tk_img = tk_img2
     os.remove("hist2.png")
 
     global noisy_image
-    img5, bins = np.histogram(noisy_image.flatten(), bins=256, range=[0,256])
+    img5, bins = np.histogram(noisy_image.flatten(), bins=256, range=[0, 256])
     # img5 = cv2.equalizeHist(noisy_image)
     plt.figure()
-    plt.plot(img5, color = 'black')
+    plt.plot(img5, color='black')
     # plt.hist(img5.ravel(), 256, [0, 255], color="black")
     plt.savefig("hist3.png")
     plt.close()
     global hist3
     hist3 = cv_imread("hist3.png")
-    tk_img3 = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(hist3, (320, 320))))
+    tk_img3 = ImageTk.PhotoImage(
+        image=Image.fromarray(cv2.resize(hist3, (320, 320))))
     canvas6.delete('all')
-    canvas6.create_image(0,0, anchor='nw', image=tk_img3)
+    canvas6.create_image(0, 0, anchor='nw', image=tk_img3)
     canvas6.tk_img = tk_img3
     os.remove("hist3.png")
     # tk_img = tk.PhotoImage(file="hist.png")
@@ -224,6 +242,7 @@ def histogram(): # 顯示直方圖
 #     # img_type = img_path.split('.')[1]  # 取得檔案類型
 #     img_save = cv2.cvtColor(img2, cv2.COLOR_BGR2RGBA)
 #     cv2.imwrite(img_path, img2)
+
 
 btn1 = tk.Button(text="選擇照片", bg="skyblue", command=show)
 btn1.config(font="微軟正黑體 15")
@@ -346,20 +365,12 @@ frame4.place(x=0, y=450)
 frame5.place(x=400, y=450)
 frame6.place(x=800, y=450)
 
-btn1.place(x=0, y=0) # select image
-btn2.place(x=200, y=0) # flip image
+btn1.place(x=0, y=0)  # select image
+btn2.place(x=200, y=0)  # flip image
 # btn3.place(x=800, y=0) # save
-btn4.place(x=100, y=0) # gray image
-btn5.place(x=605, y=0) # histogram
-btn6.place(x=300, y=0) # noise
-btn7.place(x=452, y=0) # noise
+btn4.place(x=100, y=0)  # gray image
+btn5.place(x=605, y=0)  # histogram
+btn6.place(x=300, y=0)  # noise
+btn7.place(x=452, y=0)  # noise
 
 win.mainloop()
-
-
-
-
-
-
-
-
